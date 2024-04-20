b76e70eee2519b1340ef3b87047dc1253cf050d3
Waify62 <waify62@gmail.com>
2024-04-20 23:33:34 +0500

    usifj

---
+++ app/src/main/java/com/cyberhuskies/travellersearchapp/domain/Place.java
@@ -0,0 +1,19 @@
+package com.cyberhuskies.travellersearchapp.domain;
+
+import com.google.android.gms.maps.model.LatLng;
+
+public class Place {
+
+
+    private final String city;
+
+    private final LatLng coordinates;
+
+    public Place(String city, LatLng coordinates){
+
+        this.city=city;
+        this.coordinates=coordinates;
+    }
+
+
+}
